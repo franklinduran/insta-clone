66fe72152d34e84d34823edab65a430f903ff0e4
Franklin <franklindejesusduran@gmail.com>
2021-10-20 19:51:59 -0400

    Modal centered

--- components/Modal.js
+++ components/Modal.js
@@ -62,10 +62,10 @@ function Modal() {
         <Transition.Root show={open} as={Fragment}>
             <Dialog
                 as='div'
-                className='fixed z-10 inset-0 overflow-y-auto'
+                className='fixed z-10 inset-0 overflow-y-auto mt-40'
                 onClose={setOpen}
             >
-                <div className='flex items-end justify-center center min-h-[800px] sm:min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0'>
+                <div className='flex items-end justify-center min-h-[800px] sm:min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0'>
                     <Transition.Child 
                         as={Fragment}
                         enter='ease-out duration-300'
